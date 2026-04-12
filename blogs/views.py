8de140edda5f8fdf292d3d4d87b7b9270283d6a9
Sneha Gholap <snehagholap20@gmail.com>
2026-04-12 21:42:02 +0530

    comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.shortcuts import HttpResponse
-from .models import Blog,Category 
+from django.http import HttpResponseRedirect
+from .models import Blog,Category , Comment
 from django.db.models import Q
 # Create your views here.
 
@@ -28,8 +29,23 @@ def posts_by_category(request,category_id):
 def blogs(request, slug):
   single_blog=get_object_or_404(Blog, slug=slug,status='Published')
   #print(single_blog)
+  if request.method=='POST':
+    comment=Comment()
+    comment.user=request.user
+    comment.blog=single_blog
+    comment.comment=request.POST['comment']
+    comment.save()
+    return HttpResponseRedirect(request.path_info)
+
+  #comments
+  comments=Comment.objects.filter(blog=single_blog)
+  comment_count=comments.count()
+
+
   context={
     'single_blog': single_blog,
+    'comments':comments,
+    'comment_count':comment_count,
   }
   return render(request,'blogs.html', context)
 
@@ -44,3 +60,4 @@ def search(request):
   }
   #print("Keyword==>",keyword)
   return render(request,'search.html',context)
+
